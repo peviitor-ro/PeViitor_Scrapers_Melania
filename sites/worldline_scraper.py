1c71c65e93f5b26b9e470c6448494f1bbd88d891
Melania Stefania Miron <melania23m@yahoo.com>
2023-09-25 14:49:26 +0300

    added worldline scraper

---
+++ sites/worldline_scraper.py
@@ -0,0 +1,65 @@
+#
+#
+#
+# New scraper for -> Worldline
+# Worldline page -> https://careers.worldline.com/en
+
+#
+import re
+
+from A_OO_get_post_soup_update_dec import DEFAULT_HEADERS, update_peviitor_api
+from L_00_logo import update_logo
+#
+import requests
+from bs4 import BeautifulSoup
+#
+import uuid
+
+
+def req_and_collect_data_():
+    """
+    ... this func() make a simple requests
+    and collect data from Acronis API.
+    """
+
+    response = requests.get(
+        'https://careers.worldline.com/en/search-jobs/results?ActiveFacetID=0&CurrentPage=1&RecordsPerPage=100&Distance=50&RadiusUnitType=0&Keywords=Romania&Location=&ShowRadius=False&IsPagination=False&CustomFacetName=&FacetTerm=&FacetType=0&SearchResultsModuleName=Search+Results&SearchFiltersModuleName=Search+Filters&SortCriteria=0&SortDirection=0&SearchType=1&PostalCode=&ResultsType=0&fc=&fl=&fcf=&afc=&afl=&afcf=',
+        headers=DEFAULT_HEADERS)
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    soup_data = soup.find_all('a', href=True)
+
+    lst_with_data = []
+
+    for dt in soup_data:
+        title = dt.find('h3')
+        link = dt['href']
+        lst_with_data.append({
+            "id": str(uuid.uuid4()),
+            "job_title": title.text,
+            "job_link": 'https://careers.worldline.com/' + link.split('"')[1],
+            "company": "Worldline",
+            "country": "Romania",
+            "city": "Romania"
+        })
+
+    return lst_with_data
+
+
+# update data on peviitor!
+@update_peviitor_api
+def scrape_and_update_peviitor(company_name, data_list):
+    """
+    Update data on peviitor API!
+    """
+
+    return data_list
+
+
+company_name = 'Worldline'  # add test comment
+data_list = req_and_collect_data_()
+scrape_and_update_peviitor(company_name, data_list)
+
+print(update_logo('Worldline',
+                  'https://branditechture.agency/brand-logos/wp-content/uploads/wpdm-cache/Worldline-900x0.png'
+                  ))
