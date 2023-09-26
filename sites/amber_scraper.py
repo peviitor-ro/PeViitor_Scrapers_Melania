99c1d62ddbbd173d1c4b79a9aab1f4b52d6584d7
Melania Stefania Miron <melania23m@yahoo.com>
2023-09-26 16:32:41 +0300

    addec amber scraper

---
+++ sites/amber_scraper.py
@@ -0,0 +1,67 @@
+#
+#
+#
+# New scraper for -> Amber
+# Amber page -> https://jobs.jobvite.com/amberstudiocareers/jobs/positions
+
+#
+import re
+
+from A_OO_get_post_soup_update_dec import DEFAULT_HEADERS, update_peviitor_api
+from L_00_logo import update_logo
+#
+import requests
+from bs4 import BeautifulSoup
+#
+import uuid
+
+
+def req_and_collect_data_():
+    """
+    ... this func() make a simple requests
+    and collect data from Amber API.
+    """
+
+    response = requests.get('https://jobs.jobvite.com/amberstudiocareers/jobs/positions',
+                            headers=DEFAULT_HEADERS)
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    soup_data = soup.find_all('li', class_='job-item')
+
+    lst_with_data = []
+
+    for dt in soup_data:
+        location = dt.find('div', class_='jv-job-list-location').text.strip()
+        title = dt.find('div', class_='jv-job-list-name').text.strip()
+        link = dt.find('a', class_='jv-button jv-button--hollow flex-row flex-m-space-between flex-c-center')[
+            'href']
+        if 'romania' in location.lower():
+            lst_with_data.append({
+                "id": str(uuid.uuid4()),
+                "job_title": title,
+                "job_link": 'https://jobs.jobvite.com/' + link,
+                "company": "Amber",
+                "country": "Romania",
+                "city": location.split('\n')[-2].strip().replace(',', '')
+            })
+
+    return lst_with_data
+
+
+# update data on peviitor!
+@update_peviitor_api
+def scrape_and_update_peviitor(company_name, data_list):
+    """
+    Update data on peviitor API!
+    """
+
+    return data_list
+
+
+company_name = 'Amber'  # add test comment
+data_list = req_and_collect_data_()
+scrape_and_update_peviitor(company_name, data_list)
+
+print(update_logo('Amber',
+                  'https://mma.prnewswire.com/media/1633540/Amber_Logo.jpg?w=200'
+                  ))
