80d6e2b47ac4f9b86afcd01d8eabc07912b487fb
Melania Stefania Miron <melania23m@yahoo.com>
2023-10-05 12:51:30 +0300

    changed name RSM to RSMRomania

--- sites/RSMRomania_scraper.py
+++ sites/RSMRomania_scraper.py
@@ -1,8 +1,8 @@
 #
 #
 #
-# New scraper for -> RSM
-# RSM page -> https://www.rsm.global/romania/ro/campaign/joburi-disponibile
+# New scraper for -> RSMRomania
+# RSMRomania page -> https://www.rsm.global/romania/ro/campaign/joburi-disponibile
 #
 #
 from A_OO_get_post_soup_update_dec import DEFAULT_HEADERS, update_peviitor_api
@@ -17,7 +17,7 @@ import uuid
 def req_and_collect_data_():
     """
     ... this func() make a simple requests
-    and collect data from RSM API.
+    and collect data from RSMRomania API.
     """
 
     response = requests.get('https://www.rsm.global/romania/ro/campaign/joburi-disponibile',
@@ -34,7 +34,7 @@ def req_and_collect_data_():
             "id": str(uuid.uuid4()),
             "job_title": dt.find('h2', class_='tab-pane--title').text,
             "job_link": 'https://www.rsm.global/' + link,
-            "company": "RSM",
+            "company": "RSMRomania",
             "country": "Romania",
             "city": "Bucuresti"
         })
@@ -52,10 +52,10 @@ def scrape_and_update_peviitor(company_name, data_list):
     return data_list
 
 
-company_name = 'RSM'  # add test comment
+company_name = 'RSMRomania'  # add test comment
 data_list = req_and_collect_data_()
 scrape_and_update_peviitor(company_name, data_list)
 
-print(update_logo('RSM',
+print(update_logo('RSMRomania',
                   'https://res.cloudinary.com/rsmglobal/image/fetch/t_default/f_auto/q_auto/https://www.rsm.global/romania/profiles/rsm_global_platform/themes/rsm_global_platform_2022/images/logo@2x.png'
                   ))
